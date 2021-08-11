2fb621ffe7bdfa0783b660ad480a44e44b6d9bd1
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2021-08-11 09:33:34 -0700

    app component controls the logic and view of the application at a high level.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //Equivalent to React.FC, used to create a component.
 
+// Component Declaration
+/* 
+selector: Refers to HTML tag in markup
+template: HTML file
+styleUrls: CSS 
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-crash-course';
-}
+export class AppComponent {}
